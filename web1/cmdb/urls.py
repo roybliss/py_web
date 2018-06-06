926ec5e2b7bacc1a35a8273099ad607594e4af32
luoliang <luoliang@yiqihao.com>
2018-06-06 18:16:32 +0800

    update

--- web1/cmdb/urls.py
+++ web1/cmdb/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from cmdb import views
 
 urlpatterns = [
-   path('', views.index),
+   re_path(r'^$', views.index),
 ]
