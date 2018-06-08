fb84f4a5ea6525791737948fa6425ce638e1bcc3
luoliang <luoliang@yiqihao.com>
2018-06-08 18:11:42 +0800

    update

--- web1/blog/urls.py
+++ web1/blog/urls.py
@@ -7,4 +7,6 @@ app_name = 'blog'
 urlpatterns = [
    re_path(r'^$', views.index),
    re_path(r'^article/(?P<article_id>\d+)$', views.article_page, name='article_page'),
+   re_path(r'^add/(?P<article_id>\d+)$', views.add_page, name='add_page'),
+   re_path(r'^add_action/$', views.add_action, name='add_action'),
 ]
--- web1/blog/views.py
+++ web1/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.http import HttpResponseRedirect
 from . import models
 
 
@@ -13,3 +14,15 @@ def index(request):
 def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', {'article': article})
+
+def add_page(request, article_id):
+    if str(article_id) == 0:
+        return render(request, 'blog/add_page.html')
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/add_page.html', {'article': article})
+
+def add_action(request):
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    models.Article.objects.create(title=title, content=content)
+    return HttpResponseRedirect('/blog/')
