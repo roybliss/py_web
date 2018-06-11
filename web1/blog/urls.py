8acacdb209f800ef0eb8a41737f4a1d4643c8459
luoliang <luoliang@yiqihao.com>
2018-06-11 13:14:10 +0800

    添加blog新增和修改页面

--- web1/blog/urls.py
+++ web1/blog/urls.py
@@ -7,6 +7,6 @@ app_name = 'blog'
 urlpatterns = [
    re_path(r'^$', views.index),
    re_path(r'^article/(?P<article_id>\d+)$', views.article_page, name='article_page'),
-   re_path(r'^add/(?P<article_id>\d+)$', views.add_page, name='add_page'),
-   re_path(r'^add_action/$', views.add_action, name='add_action'),
+   re_path(r'^edit/(?P<article_id>\d+)$', views.edit_page, name='edit_page'),
+   re_path(r'^edit_action/$', views.edit_action, name='edit_action'),
 ]
--- web1/blog/views.py
+++ web1/blog/views.py
@@ -15,14 +15,18 @@ def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', {'article': article})
 
-def add_page(request, article_id):
-    if str(article_id) == 0:
-        return render(request, 'blog/add_page.html')
+def edit_page(request, article_id):
+    if str(article_id) == '0':
+        return render(request, 'blog/edit_page.html')
     article = models.Article.objects.get(pk=article_id)
-    return render(request, 'blog/add_page.html', {'article': article})
+    return render(request, 'blog/edit_page.html', {'article': article})
 
-def add_action(request):
+def edit_action(request):
     title = request.POST.get('title', 'TITLE')
     content = request.POST.get('content', 'CONTENT')
-    models.Article.objects.create(title=title, content=content)
+    id = request.POST.get('article_id', '0')
+    if id == '0':
+        models.Article.objects.create(title=title, content=content)
+    else:
+        models.Article.objects.filter(pk=id).update(title=title, content=content)
     return HttpResponseRedirect('/blog/')
