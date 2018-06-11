5d084a9ff5477a93ee1d51f583748b0aeaa4011a
luoliang <luoliang@yiqihao.com>
2018-06-11 17:40:52 +0800

    Admin extends

--- web1/blog/admin.py
+++ web1/blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'create_time')
+    list_filter = ('create_time', )
+
+admin.site.register(Article, ArticleAdmin)
